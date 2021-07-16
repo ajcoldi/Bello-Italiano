feae8752af7c2fea600a8c07f62d26e496697258
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-07-16 11:10:44 +0100

    route ajustment

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ app.use(express.static('public'));
 
 const pageRoutes = require('./routes/pageRoutes');
 app.use('/', pageRoutes);
+const menuRoutes = require('./routes/menuRoutes');
+app.use('/menu', menuRoutes);
 
 app.listen(process.env.PORT || port, () => console.log(`Listening on port: ${port}`));
